1a61a7821ae3cbc8884f8e10ca1931ed14c26855
Deepika-muppana-06 <154745754+Deepika-muppana-06@users.noreply.github.com>
2025-11-26 14:02:51 +0530

    Time: 0 ms (100%), Space: 10.2 MB (93.6%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -7,13 +7,13 @@ public:
     string reverseVowels(string s) {
         int i=0,j=s.size()-1;
         while(i<j){
-            while(i<j && !isVowel(s[i]))i++;
-            while(i<j && !isVowel(s[j]))j--;
-        if(i<j){
-            swap(s[i], s[j]);
-            i++;
-            j--;
-        }
+           if(!isVowel(s[i]))i++;
+           else if(!isVowel(s[j]))j--;
+           else {
+            swap(s[i],s[j]);
+                i++;
+                j--;
+           }
         }
         return s;
     }
